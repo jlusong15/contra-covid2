7dfa90b057e2ed5c2f649c7b77e8cfe920c802c1
Jennifer Lusong <jenniferlusong@gmail.com>
2020-11-17 02:19:33 +0800

    About page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,12 +14,28 @@ export class AppComponent {
     private router: Router,
     private renderer: Renderer2
   ) {}
+
+  resetClass(el, newClass) {
+    console.log("reset", el , newClass)
+    if (el && newClass) {
+      console.log("el.getAttribute('class')", )
+      if (el.getAttribute('class')) {
+        let classes = el.getAttribute('class').split(' '); // get all classes
+        console.log("classes", classes)
+        classes.forEach((cl) => {
+          console.log("remove")
+          this.renderer.removeClass(el, cl);
+        });
+      }
+      this.renderer.addClass(el, newClass);
+    }
+  }
   
   ngOnInit(): void {
     this.router.events.subscribe((res) => {
       this.currentPath = this.router.url.replace('/','')
       this.currentPath = (this.currentPath == "") ? 'home': this.currentPath;
-      this.renderer.addClass(document.body, "body-" + this.currentPath);
+      this.resetClass(document.body, "body-" + this.currentPath)
       document.documentElement.style.height = "100%"
     })
   }
