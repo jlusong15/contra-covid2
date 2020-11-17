6ead8f6fdf593c02677ec282c94391f8e22aced5
Jennifer Lusong <jenniferlusong@gmail.com>
2020-11-17 18:34:11 +0800

    refactor step 3

--- src/app/components/preview-details/preview-details.component.ts
+++ src/app/components/preview-details/preview-details.component.ts
@@ -7,17 +7,31 @@ import { StepService } from '../../step.service';
   styleUrls: ['./preview-details.component.less']
 })
 export class PreviewDetailsComponent implements OnInit {
-  allData = {
-    step1 : {},
-    step2 : {}
+  step1 = {
+    fullName: '',
+    email: '',
+    contactNumber: '',
+    address: ''
   }
+  step2 = {
+    temperature: '',
+    feeling: '',
+    hasCough: false,
+    hasCovidContact: false,
+    additionalInfo: ''
+  }
+  allData = {
+    step1 : this.step1,
+    step2 : this.step2
+  }
+  
   constructor(private step: StepService) { }
 
   ngOnInit(): void {
-    this.step.getStep1.subscribe(data => {
+    this.step.getStep1.subscribe((data:any)=> {
       this.allData.step1 = data
     })
-    this.step.getStep2.subscribe(data => {
+    this.step.getStep2.subscribe((data:any) => {
       this.allData.step2 = data
     })
     console.log("allData", this.allData)
